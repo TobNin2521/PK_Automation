418ec6c1015a65e82c96a2dd065bc760f1a28a69
Tobias Ninz <tobias.ninz@demaclenko.com>
2022-05-05 16:37:19 +0200

    Added two new effects (fire & meteor) and added "describe" to unit test

--- relays.test.js
+++ relays.test.js
@@ -1,2 +1,7 @@
-const relays = require("./relays");
+const relays = require("./relays")(false);
 
+describe("Unit tests for relay class", () => {
+    test("Test set brightness function", () => {
+        expect(2).toBe(2);
+    });
+});
