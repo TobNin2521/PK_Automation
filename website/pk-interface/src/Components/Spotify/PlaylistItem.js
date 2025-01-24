c3606fddf743598e17eb3ca73151b1e442468d54
Tobias Ninz <tobias.ninz34@gmail.com>
2025-01-25 00:31:13 +0100

    Playlist null bug

--- website/pk-interface/src/Components/Spotify/PlaylistItem.js
+++ website/pk-interface/src/Components/Spotify/PlaylistItem.js
@@ -3,14 +3,16 @@ import './PlaylistItem.css';
 export const PlaylistItem = ({playlist, onClick}) => {
 
     return (
-        <div className="playlist" onClick={() => onClick(playlist.id)}>
-            {playlist.images !== undefined && playlist.images !== null && playlist.images.length > 0 ? <img src={playlist.images[0].url} /> : null}
-            <div className='playlist-info'>
-                {playlist.name}                
+        playlist !== null ? (
+            <div className="playlist" onClick={() => onClick(playlist.id)}>
+                {playlist.images !== undefined && playlist.images !== null && playlist.images.length > 0 ? <img src={playlist.images[0].url} /> : null}
+                <div className='playlist-info'>
+                    {playlist.name}                
+                </div>
+                <div className='playlist-tracks'>
+                    Tracks: {playlist.tracks.total}
+                </div>
             </div>
-            <div className='playlist-tracks'>
-                Tracks: {playlist.tracks.total}
-            </div>
-        </div>
+        ) : null
     );
 };
