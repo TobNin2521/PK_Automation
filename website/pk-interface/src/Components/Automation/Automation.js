7c5affef02f61734ff56efded0ea227abe25bf03
Tobias Ninz <tobias.ninz34@gmail.com>
2025-08-07 22:55:03 +0200

    Updates the IP address of the Bar light
    Updates the IP address of the Bar light to reflect the new network configuration.

--- website/pk-interface/src/Components/Automation/Automation.js
+++ website/pk-interface/src/Components/Automation/Automation.js
@@ -8,7 +8,7 @@ export const Automation = ({}) => {
         <div className="automation">
             <Relay name={"Bar Licht"} pin={33} />
             <Relay name={"Led"} pin={35}>
-                <LedControl name='Bar' address={"http://192.168.178.65"} />
+                <LedControl name='Bar' address={"http://192.168.178.107"} />
                 <LedControl name='Wand' address={"http://192.168.178.66"} />
             </Relay>
             <Relay name={"Regale"} pin={37} />
